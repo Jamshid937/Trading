75a5e50ac12bc9eb50082c02a8cc04e4cc26b87b
Jamshid937 <mirazamovj8@gmail.com>
2023-12-12 18:20:05 +0500

    no comment

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './index.css'
 import vektor from '../../Imgs/vektor.svg'
+import { useState } from 'react'
 const Collapse = () => {
+    const [collapse, setCollapce] = useState(false)
     const faqs = document.querySelectorAll(".faq");
+
+
 
 
     faqs.forEach(faq => {
@@ -12,10 +16,10 @@ const Collapse = () => {
     })
     return (
         <div>
-            <div className="faq">
+            <div className='faq'>
                 <div className="question">
                     <img src={vektor} alt="" />
-                    <h3>У меня получится, если я новичок?</h3>
+                    <h3>Есть ли у вас инженеры в компании?</h3>
                 </div>
                 <div className="ansver">
                     <p>
@@ -26,7 +30,7 @@ const Collapse = () => {
             <div className="faq">
                 <div className="question">
                     <img src={vektor} alt="" />
-                    <h3>У меня получится, если я новичок?</h3>
+                    <h3>Делаете ли вы отчистку газгольдера?</h3>
                 </div>
                 <div className="ansver">
                     <p>
@@ -37,7 +41,7 @@ const Collapse = () => {
             <div className="faq">
                 <div className="question">
                     <img src={vektor} alt="" />
-                    <h3>У меня получится, если я новичок?</h3>
+                    <h3>А вы не испортите газон у меня и соседей?</h3>
                 </div>
                 <div className="ansver">
                     <p>
@@ -48,7 +52,7 @@ const Collapse = () => {
             <div className="faq">
                 <div className="question">
                     <img src={vektor} alt="" />
-                    <h3>У меня получится, если я новичок?</h3>
+                    <h3>Вы предоставляете документы для субсидий?</h3>
                 </div>
                 <div className="ansver">
                     <p>
@@ -59,7 +63,7 @@ const Collapse = () => {
             <div className="faq">
                 <div className="question">
                     <img src={vektor} alt="" />
-                    <h3>У меня получится, если я новичок?</h3>
+                    <h3>Смогу ли я заказать во время праздников?</h3>
                 </div>
                 <div className="ansver">
                     <p>
